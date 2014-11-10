8eb23af99fcde7fb12db1bdfbed6144fb7ab4a49
developer <developer@developerdeiMac.local>
2014-11-10 17:52:38 +0800

    丁瑞

--- B2C_MMB_iOS/viewcontroller/WaitViewController.m
+++ B2C_MMB_iOS/viewcontroller/WaitViewController.m
@@ -433,7 +433,6 @@ double secondsCountDown =0;
     }
     else if (buttonIndex == 0)
     {
-        
         if ([self.tempFrom isEqualToString:@"首页在线客服"])
         {
             [self.navigationController popViewControllerAnimated:YES];

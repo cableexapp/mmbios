16538a304a6072badc81266f68bfcb3a53743c6f
App01 <app01@App01deiMac.local>
2014-12-10 11:32:35 +0800

    cx

--- B2C_MMB_iOS/Base/控件/DCFMyTextField.m
+++ B2C_MMB_iOS/Base/控件/DCFMyTextField.m
@@ -14,8 +14,8 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.layer.borderColor = [UIColor lightGrayColor].CGColor;
-        self.layer.borderWidth = 0.5f;
+        self.layer.borderColor = [UIColor colorWithRed:153.0/255.0 green:152.0/255.0 blue:155.0/255.0 alpha:1.0].CGColor;
+        self.layer.borderWidth = 1.0f;
         [self setBackgroundColor:[UIColor whiteColor]];
         self.layer.masksToBounds = YES;
 //        
